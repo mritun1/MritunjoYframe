ed964aa8f6bb439ee47a6b13fa0fdf345b0c2fb7
mritun1 <103233905+mritun1@users.noreply.github.com>
2022-06-06 10:06:36 +0530

    Delete assets/view/layout directory
    no need

--- assets/view/layout/menu-home.php
+++
@@ -1,5 +0,0 @@
-<div class="page_title container-xs">
-    <button go_href="/" class="btn-home active"><i class="fa-solid fa-house"></i> Home</button>
-    <button go_href="/new-problems" class="btn-new"><i class="fa-solid fa-chart-simple"></i> Newest</button>
-    <button go_href="/unsolved" class="btn-unsolved"><i class="fa-solid fa-circle-exclamation"></i> Unsolved</button>
-</div>
